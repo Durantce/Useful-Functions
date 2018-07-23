580c487f6ae807b96c304606c61c1e061dde0b54
Durantce <41088511+Durantce@users.noreply.github.com>
2018-07-23 10:00:04 -0400

    Update Useful.py

--- Useful.py
+++ Useful.py
@@ -59,3 +59,43 @@ def get_population_by_fips(fips):
     '''
     
     return list(population_frame[population_frame['Fips'] == fips]['B01001_001E'])[0]
+
+
+
+# Plotting statewide data --> frame should have two columns ('code': state abbreviations ('OH') and 'temperature': temperature data
+import plotly.plotly as py
+import pandas as pd
+
+for col in df.columns:
+    df[col] = df[col].astype(str)
+
+scl = [[0.0, 'rgb(242,240,247)'],[0.2, 'rgb(218,218,235)'],[0.4, 'rgb(188,189,220)'],\
+            [0.6, 'rgb(158,154,200)'],[0.8, 'rgb(117,107,177)'],[1.0, 'rgb(84,39,143)']]
+
+data = [ dict(
+        type='choropleth',
+        colorscale = scl,
+        autocolorscale = False,
+        locations = frame['code'],
+        z = frame['temperature'].astype(float),
+        locationmode = 'USA-states',
+        marker = dict(
+            line = dict (
+                color = 'rgb(255,255,255)',
+                width = 2
+            ) ),
+        colorbar = dict(
+            title = "Degrees (F)")
+        ) ]
+
+layout = dict(
+        title = 'Temperature by State<br>(Hover for breakdown)',
+        geo = dict(
+            scope='usa',
+            projection=dict( type='albers usa' ),
+            showlakes = True,
+            lakecolor = 'rgb(255, 255, 255)'),
+             )
+    
+fig = dict( data=data, layout=layout )
+py.iplot( fig, filename='d3-cloropleth-map' )
